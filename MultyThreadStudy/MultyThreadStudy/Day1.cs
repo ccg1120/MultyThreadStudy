f384fbafe32314ad805599bd5ddf958faee9dcef
ccg1120 <ccg.1120@daum.net>
2018-06-05 14:06:20 +0900

    49p DeadLock 문제

--- MultyThreadStudy/MultyThreadStudy/Day1.cs
+++ MultyThreadStudy/MultyThreadStudy/Day1.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
+/// <summary>
+/// Deadlock 오류 및 해결 방안 Monitor 생성자 잠그기
+/// </summary>
 namespace MultyThreadStudy
 {
     class Day1
     {
+
+        
+
+
+
+        public static void LockTooMuch(object lock1, object lock2)
+        {
+            lock(lock1)
+            {
+                Thread.Sleep(1000);
+                lock (lock2)
+                {
+
+                }
+
+            }
+        }
     }
+
+   
 }
--- MultyThreadStudy/MultyThreadStudy/Program.cs
+++ MultyThreadStudy/MultyThreadStudy/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 /// <summary>
 /// 멀티스레드를 공부하기위한 프로젝트
@@ -15,6 +17,39 @@ namespace MultyThreadStudy
     {
         static void Main(string[] args)
         {
+            object lock1 = new object();
+            object lock2 = new object();
+
+            new Thread(() => Day1.LockTooMuch(lock1, lock2)).Start();
+
+            lock(lock2)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Monitor.try enter allow not to get ");
+                if(Monitor.TryEnter(lock1,TimeSpan.FromSeconds(5)))
+                {
+                    Console.WriteLine("resoucr succesfully !!");
+                }
+                else
+                {
+                    Console.WriteLine("time out resource");
+                }
+            }
+
+            new Thread(()=> Day1.LockTooMuch(lock1,lock2)).Start();
+
+            Console.WriteLine("_________________________________");
+
+            lock (lock2)
+            {
+                Console.WriteLine("Monitor.try enter allow not to get ");
+                Thread.Sleep(1000);
+                lock (lock1)
+                {
+                    Console.WriteLine("next resource!!");
+                }
+
+            }
 
         }
     }
