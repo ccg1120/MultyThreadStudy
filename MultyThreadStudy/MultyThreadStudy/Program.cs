00901d02fb76d4207eae11e502d3ee314ecd591e
ccg1120 <ccg.1120@daum.net>
2018-06-05 09:57:55 +0900

    First Project Commit
    ignore Setting, Create project(C# Console)

---
+++ MultyThreadStudy/MultyThreadStudy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 멀티스레드를 공부하기위한 프로젝트
+/// 시작일 : 2018년 06월 05일
+/// 종료일 : 
+/// </summary>
+namespace MultyThreadStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
